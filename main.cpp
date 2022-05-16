7cb041fa4258bfaea9a095576d21dff3ce48b5a3
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-05-16 16:50:41 +0100

    Commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+int main() {
+
+    sf::RenderWindow window(sf::VideoMode(600, 800), "My Window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        sf::RectangleShape rect;
+        rect.setSize(sf::Vector2f(500, 500));
+        window.draw(rect);
+        window.display();
+
+    }
+
+    return 0;
+}
